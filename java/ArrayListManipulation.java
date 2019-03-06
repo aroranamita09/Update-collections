969cbfacef92256ad7f249fc389b6635ef60a8d1
Namita <aroranamita33@gmail.com>
2019-03-06 17:30:15 +0530

    completed

--- java/ArrayListManipulation.java
+++ java/ArrayListManipulation.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListManipulation {
-    public static List<String> manipulateList(List<String> oldList, String oldStr, String newStr){
+     List<String> manipulateList(List<String> oldList, String oldStr, String newStr){
         
         return oldList;
     }
 
-    public List<String> emptyListNew(List<String> oldList){
+     List<String> emptyListNew(List<String> oldList){
         
         return oldList;
     }
